40739e6a92e712523fa1542475785f4a2a505578
CodeWizard234 <145546222+CodeWizard234@users.noreply.github.com>
2024-08-30 18:46:46 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score=false;
+
+// console.log(typeof score);
+
+// 2nd method for checking the type
+
+// console.log(typeof(score));
+
+
+// let valueInNumber =Number(score)
+
+// console.log( valueInNumber)
+
+
+//"34" = 34
+
+//"34abc" =>NaN
+// true =>1; false =>
+    
+
+    let isLoggedIn = "AQUA"
+
+    let booleanIsLoggedIn =Boolean(isLoggedIn)
+
+    console.log(booleanIsLoggedIn)
+
+
+    // 1 =>true ; 0=>false
+
+    // "" =>false
+    // "AQUA" =>True
+
+
+    //for string 
+
+    let someNumber =22;
+
+    let stringNumber =String(someNumber)
+
+    console.log(typeof stringNumber);
+
