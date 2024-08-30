053577173dd72fce042c542991ab82c2d7658fdd
CodeWizard234 <145546222+CodeWizard234@users.noreply.github.com>
2024-08-30 17:28:15 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all Js code as newer version.
+
+// alert(3 + 3)//we are using alert in codespace  node.js , not a browser
+
+console.log( 3 
++ 3
+) // code readability should be high
+
+
+console.log("AQUA")
+let name="aqua";
+let age=21;
+let isLoggedIn=false;
+
+//number =>2 to the power of 53
+//bigint 
+//string =>""
+//boolean =>true / false
+//null =>standalone value
+//undefined => not asigned value
+//symbol => unique
+
+// Object
+
+console.log(typeof null)
+console.log(typeof undefined)
