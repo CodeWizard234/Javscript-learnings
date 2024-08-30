2d92b96c373394de12b32bea617dc111025554d7
CodeWizard234 <145546222+CodeWizard234@users.noreply.github.com>
2024-08-30 17:52:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=12345;
+let accountEmail="aman.pathak@gmail.com"
+var accountPassword="3334"
+accountCity="Mumbai"
+let aqua;
+
+
+accountEmail="ajahdfa@gjaa.com"
+accountPassword="2343243"
+accountCity="Dhaka"
+/* Prepare not to use var 
+because of  block space and functional space
+*/
+// accountId=2;
+console.log("accountId");
+console.table([ aqua, accountPassword, accountId, accountCity , accountEmail])
+
