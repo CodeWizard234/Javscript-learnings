a3fbde4a75c7f2fd9f2c0a2ee2bfd9d86fa1e875
CodeWizard234 <145546222+CodeWizard234@users.noreply.github.com>
2024-08-31 16:30:04 +0000

    datatypes-summary

---
+++ 01_basics/04_datatypes-summary.js
@@ -0,0 +1,45 @@
+// Primitive
+
+
+
+// 7 Types : string , Number , boolean , null , undefined Symbol(to make a value unique) , BigInt
+
+
+const score =100
+
+const scoreValue=100.3;
+
+const isLoggedIn=false
+
+const outsideTemp= null
+
+let userEmail;
+
+const id = Symbol('234')
+const anotherId = Symbol('2343')
+
+// console.log(id===anotherId);
+
+
+// const bigNumber = 2343432552343243423432n
+
+// Referrence type : Non-Primitive 
+
+
+// Arrays , objects ( browser events to master javascript) , Functions
+
+
+const heros = ["shaktiman" , "naagraj" , "doga"]
+{
+    name:"AQUA",
+    age=23;
+}
+
+
+const myFunction = function(){
+    console.log("Hellow");
+
+}
+
+
+console.log(typeof id);
