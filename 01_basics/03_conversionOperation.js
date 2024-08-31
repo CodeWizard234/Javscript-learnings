40ccbbdc0411785ee4e6d971b12152fb15590076
CodeWizard234 <145546222+CodeWizard234@users.noreply.github.com>
2024-08-31 16:30:23 +0000

    conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -75,3 +75,22 @@
     ++gameCounter;
     
     console.log(gameCounter);
+
+
+    console.log("2" > 1);
+    console.log("02"> 1);
+
+    console.log(null > 0);
+    console.log(null ==0);
+    console.log(null <=0);
+
+
+    console.log(undefined ==0);
+
+    console.log(undefined >=0);
+
+
+    // for tripe equals
+
+    console.log("2" === 2);
+    
