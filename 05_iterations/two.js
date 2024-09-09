14979345e70be509f09fe5d5b60e620c1ce2c923
CodeWizard234 <145546222+CodeWizard234@users.noreply.github.com>
2024-09-09 15:44:33 +0000

    while loop and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,39 @@
+
+// while(2==2){
+
+// }
+
+
+let i =0;
+
+while(i<=10){
+    // console.log(`Value of i is ${i}`);
+    i = i + 2;
+}
+
+
+let myArray = ["flash" , "batman", "superman"];
+
+let arr = 0
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`)
+    arr = arr +1;
+}
+
+
+// do while loop in js
+
+
+let score =1;
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <=10);
+let number =11;
+
+do {
+    console.log(`Number is ${number}`);
+    number++;
+} while (number <=10);
+
